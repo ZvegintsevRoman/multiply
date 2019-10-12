c0cb997c03c4730e25c48d96171a1674b5acb197
ZvegintsevRoman <zvegintsev.roma@yandex.by>
2019-10-12 21:07:59 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  let numA = first.split('');
+  let numB = second.split('');
+  let stack = [];
+  let result = [];
+
+  for (let i = 0; numA[i]; i++) {
+    for (let j = 0; numB[j]; j++) {
+      if (!stack[i + j]) stack[i + j] = 0;      
+      stack[i + j] += numA[i] * numB[j];
+    }
+  }
+
+  stack = stack.reverse().map(item => item.toString());
+	
+  while (stack.length > 1) {        
+    result.push(stack[0].slice(-1));
+    if (stack[0].length === 1) { 
+      stack.shift();
+    }
+    else { 
+      stack[1] = (parseInt(stack[1]) + parseInt(stack[0].slice(0, -1))).toString();
+      stack.shift();
+    }
+  }
+  
+  result = stack[0] + result.reverse().join('');
+
+  return result;
 }
